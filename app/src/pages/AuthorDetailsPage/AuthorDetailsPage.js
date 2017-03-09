9bfc6190fde19473cb5e2c79e3f940d7257d2ac5
Ian Deans <ideans715@gmail.com>
2017-03-09 14:51:10 -0800

    Add AuthorDetails component that fetches data from database

--- app/src/pages/AuthorDetailsPage/AuthorDetailsPage.js
+++ app/src/pages/AuthorDetailsPage/AuthorDetailsPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { AuthorDetails } from '../../components/'
 
 import './AuthorDetailsPage.css'
 
@@ -6,6 +7,7 @@ const AuthorDetailsPage = props => {
 
   return (
     <div className="author-details page">
+      <AuthorDetails id={ props.params.id }/>
       <h1>Things</h1>
     </div>
   )
