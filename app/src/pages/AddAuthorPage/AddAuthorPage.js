312dc075f94e7dc5a15267a955faf3bf455baaeb
John Ware <contact@johndware.com>
2017-03-09 16:40:23 -0800

    add author page components

---
+++ app/src/pages/AddAuthorPage/AddAuthorPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { AddBookForm } from '../..components/'
+
+import '.AddAuthorPage.css'
+
+const AddAuthor = props => {
+
+    return (
+
+      <div className="add-author-page">
+        <h1>ADD AUTHOR</h1>
+        <AddAuthorForm />
+      </div>
+    )
+}
+
+export default AddAuthorPage
